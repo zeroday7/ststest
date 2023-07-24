f6d3b05dfae8d4ba72214f3d340b82dd8cecbba8
gdj <gdj@DESKTOP-TBPNUD3>
2023-07-24 09:34:52 +0900

    ststest 초기화

---
+++ ststest/src/main/java/com/goodee/ststest/StstestApplication.java
@@ -0,0 +1,13 @@
+package com.goodee.ststest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StstestApplication.class, args);
+	}
+
+}
---
+++ ststest/src/test/java/com/goodee/ststest/StstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodee.ststest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StstestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
